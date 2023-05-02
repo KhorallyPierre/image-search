cfa9575f4ae6644e09a497b87744c6f9b4dfc618
Khorally-s <kvpierre@gmail.com>
2023-05-02 14:02:52 -0400

    return specifications

--- src/api.js
+++ src/api.js
@@ -2,18 +2,18 @@
 import axios from 'axios';
 
 // async keyword is just a requirement to use await
-const searchImages = async() => {
+const searchImages = async(term) => {
     // the await keyword tells JS to wait for response from api before returning / console.logging response
    const response = await axios.get('https://api.unsplash.com/search/photos', {
         headers: {
             Authorization: 'Client-ID 6GH_E975ZSI7pbAz8rBmp93VZPhwNNFabUq61E5JEzM',
         },
         params: {
-            query: 'cars',
+            query: term,
         },
     });
-    console.log(response)
-    return response;
+    console.log(response.data.results)
+    return response.data.results  ;
 };
 
 export default searchImages;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import searchImages from './api';
-// test to see application is running as expected.
-searchImages();
+
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
