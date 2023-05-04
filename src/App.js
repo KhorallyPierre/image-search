c5da83c77e9ab39b17eb083075cd9038c593b9e3
Khorally-s <kvpierre@gmail.com>
2023-05-04 11:51:09 -0400

    api response

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import SearchBar from './components/SearchBar'
+import SearchBar from './components/SearchBar';
+import searchImages from './api';
 
 function App() {
     const handleSubmit = (term) => {
-console.log('do a search for', term);
-        SearchBar(term)
+        searchImages(term);
+        
     }
     return <div> 
         <SearchBar onSubmit={handleSubmit}/>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,18 +1,31 @@
-function SearchBar() {
+import {useState} from 'react';
+
+
+function SearchBar({onSubmit}) {
+    const [term, setTerm] = useState('');
+
 
     const handleFormSubmit = (event) => {
         // this prevents the page from reloading so that the console log gets rendered
         event.preventDefault();
-        console.log('I need to tell the parent about some data')
+
+        onSubmit(term);
     }
-    return <div>
+    const handleChange = (event) => {
+        setTerm(event.target.value)
+    }
+    return (
+    <div>
         {/* submit events are automatically triggered with forms in the browser */}
         <form onSubmit={(handleFormSubmit)}>
-            <input />
+            Confirm your search: {term} <br/>
+            <input value={term} onChange={handleChange}/>
+            {term < 3 &&'term must be longer'}
         </form>
 
 
     </div>
+    )
 }
 
 export default SearchBar
